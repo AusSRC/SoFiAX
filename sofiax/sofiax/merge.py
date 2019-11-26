70cba1cf4f0652164d2b1e58e16af50f27b7c111
davepallot <dave.pallot@uwa.edu.au>
2019-11-26 15:40:04 +0800

    Adjust x, y, z to absolute terms based on region applied. Add more logging.

--- sofiax/sofiax/merge.py
+++ sofiax/sofiax/merge.py
@@ -4,6 +4,7 @@ import random
 import aiofiles
 import xmltodict
 import configparser
+import logging
 
 from datetime import datetime
 
@@ -49,12 +50,10 @@ def sanity_check(flux: tuple, spatial_extent: tuple, spectral_extent: tuple, san
     max_diff = abs(max1 - max2) * 100 / ((abs(max1) + abs(max2)) / 2)
     min_diff = abs(min1 - min2) * 100 / ((abs(min1) + abs(min2)) / 2)
     if max_diff > max_extent:
-        print('spectral max failed')
         # require manual separation, add ref to UnresolvedDetection
         return False
 
     if min_diff > min_extent:
-        print('spectral min failed')
         # require manual separation, add ref to UnresolvedDetection
         return False
 
@@ -124,7 +123,7 @@ async def match_merge_detections(conn, obs_name: str, run_name: str, params: dic
         sofia_boundary = [int(i) for i in params['input.region'].split(',')]
         run_date_datetime = datetime.strptime(run_date, '%a, %d %b %Y, %H:%M:%S')
 
-        instance_id = await db_instance_insert(conn, run_id, run_date_datetime, sofia_boundary,
+        instance_id = await db_instance_insert(conn, run_id, run_date_datetime, output_filename, sofia_boundary,
                                                None, sofia_reliability, None, json.dumps(params))
 
         for _, j in enumerate(o['VOTABLE']['RESOURCE']['TABLE']['DATA']['TABLEDATA']['TR']):
@@ -139,12 +138,19 @@ async def match_merge_detections(conn, obs_name: str, run_name: str, params: dic
             for i in range(1, len(detection)):
                 detection[i] = float(detection[i])
 
+            # adjust x, y, z to absolute terms based on region applied
+            detection[1] = detection[1] + sofia_boundary[0]
+            detection[2] = detection[2] + sofia_boundary[2]
+            detection[3] = detection[3] + sofia_boundary[4]
+
             async with conn.transaction():
                 result = await db_source_match(conn, run_id, detection)
                 result_len = len(result)
                 if result_len == 0:
+                    logging.info(f"No duplicates, Name: {detection[0]}")
                     await db_detection_insert(conn, instance_id, detection)
                 else:
+                    logging.info(f"Duplicates, Name: {detection[0]} Details: {result_len} hit(s)")
                     resolved = False
                     for db_detect in result:
                         flux = (detection[13], db_detect['f_sum'])
@@ -157,14 +163,18 @@ async def match_merge_detections(conn, obs_name: str, run_name: str, params: dic
                             detect_flag = detection[15]
                             db_detect_flag = db_detect['flag']
                             if detect_flag == 0 and db_detect_flag == 4:
+                                logging.info(f"Replacing, Name: {detection[0]} Details: flag 4 with flag 0")
                                 await db_delete_detection(conn, db_detect['id'])
                                 await db_detection_insert(conn, instance_id, detection, db_detect['unresolved'])
                             elif detect_flag == 0 and db_detect_flag == 0 or detect_flag == 4 and db_detect_flag == 4:
                                 if bool(random.getrandbits(1)) is True:
+                                    logging.info(f"Replacing, Name: {detection[0]} Details: flag 0 with "
+                                                 f"flag 0 or flag 4 with flag 4")
                                     await db_delete_detection(conn, db_detect['id'])
                                     await db_detection_insert(conn, instance_id, detection, db_detect['unresolved'])
                             resolved = True
                             break
                     if resolved is False:
+                        logging.info(f"Not Resolved, Name: {detection[0]} Details: Setting to unresolved")
                         await db_detection_insert(conn, instance_id, detection, True)
                         await db_update_detection_unresolved(conn, True, [i['id'] for i in result])
