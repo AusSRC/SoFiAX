6b74a86fe9978c7fd59f5474c3b3c09a6b5129cc
davepallot <dave.pallot@uwa.edu.au>
2020-02-26 13:56:25 +0800

    Redirect to admin from root.

--- sofiax/web/sofiax_web/sofiax_web/urls.py
+++ sofiax/web/sofiax_web/sofiax_web/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.views.generic.base import RedirectView
 
 admin.site.site_header = "WALLABY - the ASKAP HI All-Sky Survey"
 admin.site.site_title = "WALLABY Portal"
 admin.site.index_title = "Welcome to the WALLABY Portal"
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     path('admin/', admin.site.urls),
 ]
