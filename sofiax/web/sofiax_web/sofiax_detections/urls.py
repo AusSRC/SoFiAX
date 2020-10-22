c171efea3342174425eb11958c79d0f204fd344a
davepallot <dave.pallot@uwa.edu.au>
2020-10-22 11:53:29 +0800

    Remove db and web services

--- sofiax/web/sofiax_web/sofiax_detections/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('detection_products', views.detection_products, name='detection_products'),
-    path('instance_products', views.instance_products, name='instance_products'),
-    path('catalog', views.run_catalog, name='run_catalog')
-]
