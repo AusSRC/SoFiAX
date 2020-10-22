925172adc51636d93a70cf81730ae50fa3020334
davepallot <dave.pallot@uwa.edu.au>
2020-10-22 12:32:58 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='SoFiAX',
+    version='0.1.0',
+    author='',
+    author_email='',
+    packages=['sofiax'],
+    url='',
+    license='LICENSE.txt',
+    description='SoFiAX',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "aiofiles",
+        "asyncpg",
+        "xmltodict",
+        "astropy"
+    ],
+    python_requires='>3.6',
+    entry_points={
+        'console_scripts': ['sofiax=sofiax.sofiax:main'],
+    }
+)
