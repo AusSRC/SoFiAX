0580a0f643828f21163023e65078f83b2316ce7c
Austin Shen <austin.shen@csiro.au>
2021-03-18 15:32:53 +0800

    first test

---
+++ tests/tests_sofiax_arguments.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestSoFiAXArguments(TestCase):
+    """!Test class for SoFiAX arguments.
+
+    """
+    def test_always_passes(self):
+        self.assertTrue(True)
