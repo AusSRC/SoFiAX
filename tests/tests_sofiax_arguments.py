6184c0e13927d28aaf4830dba15b9285706501e8
Austin Shen <austin.shen@csiro.au>
2021-04-12 16:13:18 +0800

    removing tests

--- tests/tests_sofiax_arguments.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-
-class TestSoFiAXArguments(TestCase):
-    """!Test class for SoFiAX arguments.
-
-    """
-    def test_always_passes(self):
-        self.assertTrue(True)
